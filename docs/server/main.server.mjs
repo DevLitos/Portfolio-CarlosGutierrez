b34c6ad4b1b2497ab85dfa1fcd9f8f7e75ec1e79
DevLitos <carlos.gutierrez12@alu.uclm.es>
2025-09-12 18:23:24 +0200

    Act change in footer

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-UJXGFH7M.mjs";import"./chunk-KJY5EQ2U.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-JNDANNFN.mjs";import"./chunk-KJY5EQ2U.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
