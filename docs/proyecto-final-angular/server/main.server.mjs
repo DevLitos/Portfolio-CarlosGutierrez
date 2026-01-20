16e5b27e2e466b4cb9d5a424999a4664f9e27320
DevLitos <carlos.gutierrez12@alu.uclm.es>
2026-01-20 10:48:04 +0100

    Update docs

---
+++ docs/proyecto-final-angular/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-QZSWUH74.mjs";import"./chunk-KJY5EQ2U.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-JNDANNFN.mjs";import"./chunk-KJY5EQ2U.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
